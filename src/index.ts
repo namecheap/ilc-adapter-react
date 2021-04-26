1d02a324433360cfe1916f99d311bede7ad37dc0
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-04-26 16:43:13 +0300

    fix: fallback to CommonJS in dist for simplified usage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import singleSpaReact from './ilc-adapter-react';
+import ilcAdapterReact from './ilc-adapter-react';
 
 export type { AppLifecycleFnProps, AppWrapperLifecycleFnProps, ParcelLifecycleFnProps, LifeCycles } from 'ilc-sdk/app';
 
-export default singleSpaReact;
+export default ilcAdapterReact;
