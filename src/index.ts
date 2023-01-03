16188afa134a56ec72697a5960ab606a57d259a5
Artyom Zakharchenko <artyom.zakharchenko@namecheap.com>
2023-01-03 19:00:22 +0000

    chore: export ilc-sdk interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+import IlcSdk, { AppAssets } from 'ilc-sdk';
 import ilcAdapterReact from './ilc-adapter-react';
 
-export type { AppLifecycleFnProps, AppWrapperLifecycleFnProps, ParcelLifecycleFnProps, LifeCycles } from 'ilc-sdk/app';
+export type {
+    AppLifecycleFnProps,
+    AppWrapperLifecycleFnProps,
+    ParcelLifecycleFnProps,
+    LifeCycles,
+    Render404,
+    ErrorHandler,
+    IIlcAppSdk,
+} from 'ilc-sdk/app';
+
+
+export { IlcSdk, AppAssets };
+
 
 export default ilcAdapterReact;
